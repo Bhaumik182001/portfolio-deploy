91d1903d07db4062ec948457e28601099210b0b4
Bhaumik182001 <bhaumik182001@gmail.com>
2022-08-04 02:14:59 -0400

    f

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const withAnalyzer = require("@next/bundle-analyzer")({
+  enabled:
+    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    };
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\b(?:graphql|graphql-request)\b/gi,
+        use: "null-loader",
+      });
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = withAnalyzer(nextConfig);
